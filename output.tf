e45a7613b443cd8dea6f84e4f1856818fa793bd5
saipranayteja <pranayteja.ch@algonox.com>
2023-04-21 15:41:55 +0530

    go

--- output.tf
+++ output.tf
@@ -1,12 +1,3 @@
-output "public-ip"{
+output "public_ip"{
     value=aws_spot_instance_request.name_2.public_ip
-}
-
-
-output "jenkins_user"{
-    value=aws_ssm_parameter.jenkins_user.value
-}
-
-output "jenkins_pass"{
-    value=aws_ssm_parameter.jenkins_pass.value
 }
